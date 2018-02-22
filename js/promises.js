c75d7d69d794c5b49fd3bec7d09951ce5376c21a
davidalex620 <spand3r@gmail.com>
2018-02-22 10:20:55 -0600

    adding js/promises.js file

--- js/promises.js
+++ js/promises.js
@@ -1,27 +1,37 @@
+/**
+ *
+ * Write a function named wait that accepts a number as a parameter, and returns
+ * a promise that resolves after the passed number of milliseconds.
+ */
 
-
-
-function wait(input) {
+function wait(timer) {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
-            if (true) {
-                resolve();
-            } else {
-                reject();
-            }
-        }, input)
+            resolve(`Your promise resolved after ${timer} milliseconds`);
+        }, timer)
     });
 }
 
-wait(1000).then(() => console.log('You\'ll see this after 1 second'));
-wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
+wait(1000).then((data) => console.log(data));
+wait(3000).then((data) => console.log(data));
 
 
 
 
+
+
+
+
+
+
+/**
+ *
+ * Create a function that accepts a github username, and returns a promise that
+ * resolves with the date of the last commit that user made.
+ */
 
 function getLastCommit(username){
-    return fetch(`https://api.github.com/users/${username}/events`, {headers: {'Authorization': 'token e0ca02f8230f501712fb77e8d6eb9b7a6a53fd3a'}})
+    return fetch(`https://api.github.com/users/${username}/events`, {headers: {'Authorization': 'token $API_KEY'}})
         .then((response) => response.json())
         .then(data => {
             let lastCommit = data[0].created_at.substring(0, 10);
