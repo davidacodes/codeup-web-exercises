674e7f9a2e68b2f930c4f916e82e8d3f73930041
davidalex620 <spand3r@gmail.com>
2018-02-21 21:48:44 -0600

    completeing promises exercise

--- js/promises.js
+++ js/promises.js
@@ -20,4 +20,20 @@ wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
 
 
 
-e0ca02f8230f501712fb77e8d6eb9b7a6a53fd3a
+function getLastCommit(username){
+    return fetch(`https://api.github.com/users/${username}/events`, {headers: {'Authorization': 'token e0ca02f8230f501712fb77e8d6eb9b7a6a53fd3a'}})
+        .then((response) => response.json())
+        .then(data => {
+            let lastCommit = data[0].created_at.substring(0, 10);
+            console.log(`${username}'s last commit was on: ${lastCommit}`)
+        })
+        .catch(() => console.error('BAD REQUEST!'));
+}
+
+
+getLastCommit('davidalex620');
+
+
+
+
+
